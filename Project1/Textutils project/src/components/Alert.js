bc83ff365ca378ecad07999bc0ed641bfeed91e1
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-05-12 06:47:10 +0500

    My first classbase project
    My first classbase project

---
+++ Project1/Textutils project/src/components/Alert.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Alert(props) {
+
+    const captilize =(word)=>{
+     const lower = word.toLowerCase();
+     return   lower.charAt(0).toUpperCase() +lower.slice(1);
+    }
+  return (
+  props.alert && 
+  (
+  <div className={`alert alert-${props.alert.type} alert-dismissible fade show role="alert"`}>
+  <strong>{captilize(props.alert.type)}</strong> :{props.alert.message}
+
+</div>
+)
+  )
+}
